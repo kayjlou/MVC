399d59d1b23acbffbcf7d265ad0f16e8104ca87d
Katie Lewis <klewi314@gmail.com>
2019-05-13 19:36:50 -0700

    Added requires and functions

---
+++ controllers/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  //Here are functions that contain mysql queries
+  getMovies: _ => {},
+  getMovie: _ => {},
+  addMovie: _ => {},
+  updateMovie: _ => {},
+  deleteMovie: _ => {}
+};
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,26 @@
+//import functions written in controllers index
+const {
+  getMovies,
+  getMovie,
+  addMovie,
+  updateMovie,
+  deleteMovie
+} = require("../controllers");
+
 module.exports = app => {
   // Get all movies
   app.get("/movies", (req, res) => {});
-
+  getMovies();
   // Get 1 movie -- could say t for title
   app.get("/movies/:t", (req, res) => {});
+  getMovie();
+  //Post a movie
+  app.post("/movies", (req, res) => {});
+  addMovie();
+  //Put a movie -- use id because updating specific
+  app.put("/movie/:id", (req, res) => {});
+  updateMovie();
+  //Delete movie
+  app.delete("/movies/:id", (req, res) => {});
+  deleteMovie();
 };
