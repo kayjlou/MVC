9a456e2827518ad15a9e35991098d7c16fea15b9
Katie Lewis <klewi314@gmail.com>
2019-05-13 19:40:40 -0700

    Added sequel queries

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,18 @@
+const db = require("../config");
+
 module.exports = {
   //Here are functions that contain mysql queries
-  getMovies: _ => {},
-  getMovie: _ => {},
-  addMovie: _ => {},
-  updateMovie: _ => {},
+  getMovies: _ => {
+    db.query("SELECT * FROM movies");
+  },
+  getMovie: _ => {
+    db.query("SELECT * FROM movies WHERE ?");
+  },
+  addMovie: _ => {
+    "INSERT INTO movies SET ?";
+  },
+  updateMovie: _ => {
+    "UPDATE movies SET ? WHERE ?";
+  },
   deleteMovie: _ => {}
 };
