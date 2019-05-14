49014778a6e1ad6e42127bec2a5471af1a7f604a
Katie Lewis <klewi314@gmail.com>
2019-05-13 19:50:02 -0700

    Changed to async

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,28 @@
+//Import db connection
 const db = require("../config");
 
 module.exports = {
-  //Here are functions that contain mysql queries
-  getMovies: _ => {
-    db.query("SELECT * FROM movies");
+  async getMovies() {
+    let response = await new Promise((resolve, reject) => {
+      db.query("SELECT * FROM movies", (e, movies) => {
+        //What do we do with movies
+        if (e) reject(e);
+        //Gives function value
+        resolve(movies);
+      });
+    });
+    return response;
   },
+  //Here are functions that contain mysql queries
+  // getMovies: _ => {
+
+  //   db.query("SELECT * FROM movies", (e, movies) => {
+  //     //What do we do with movies
+  //     if (e) throw e;
+  //     //Gives function value
+  //     return movies;
+  //   });
+  // },
   getMovie: _ => {
     db.query("SELECT * FROM movies WHERE ?");
   },
@@ -14,5 +32,7 @@ module.exports = {
   updateMovie: _ => {
     "UPDATE movies SET ? WHERE ?";
   },
-  deleteMovie: _ => {}
+  deleteMovie: _ => {
+    "DELETE FROM movies WHERE ?";
+  }
 };
--- routes/index.js
+++ routes/index.js
@@ -9,18 +9,30 @@ const {
 
 module.exports = app => {
   // Get all movies
-  app.get("/movies", (req, res) => {});
-  getMovies();
+  app.get("/movies", (req, res) => {
+    //Had to write as async function as res.json would not wait for query to be completed
+    getMovies()
+      .then(movies => res.json(movies))
+      .catch(e => console.log(e));
+  });
+
   // Get 1 movie -- could say t for title
-  app.get("/movies/:t", (req, res) => {});
-  getMovie();
+  app.get("/movies/:t", (req, res) => {
+    getMovie();
+  });
+
   //Post a movie
-  app.post("/movies", (req, res) => {});
-  addMovie();
+  app.post("/movies", (req, res) => {
+    addMovie();
+  });
+
   //Put a movie -- use id because updating specific
-  app.put("/movie/:id", (req, res) => {});
-  updateMovie();
+  app.put("/movie/:id", (req, res) => {
+    updateMovie();
+  });
+
   //Delete movie
-  app.delete("/movies/:id", (req, res) => {});
-  deleteMovie();
+  app.delete("/movies/:id", (req, res) => {
+    deleteMovie();
+  });
 };
